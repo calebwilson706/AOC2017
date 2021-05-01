5293aa9d434d4b598e5d59809269c5a1c0d61f22
Caleb Wilson <calebwilson706@gmail.com>
2021-05-01 21:39:12 +0100

    day 12 changes in haskell solution

--- 2017haskellSolutions/src/Day12.hs
+++ 2017haskellSolutions/src/Day12.hs
@@ -13,7 +13,7 @@ part1 inputLines = print(length(getChildren("0",communications,[])))
         where communications = getInitialAdjacencyLists inputLines
 
 part2 :: [String] -> IO ()
-part2 inputLines = print(foldr (curry changeStateOfConnectionsAfterGrouping) (0, communications) [0..1999])
+part2 inputLines = print(fst (foldr (curry changeStateOfConnectionsAfterGrouping) (0, communications) [0..1999]))
         where communications = getInitialAdjacencyLists inputLines
 
 changeStateOfConnectionsAfterGrouping :: (Int,(Int,[(String,[String])])) -> (Int,[(String,[String])])
@@ -21,24 +21,24 @@ changeStateOfConnectionsAfterGrouping (currentNumber, (totalGroups, connections)
         where stringOfParent = show currentNumber
               validGroup = any (\connection -> isKeyEqualToDesiredKey(stringOfParent, connection)) connections
               foundGroupOfCurrentNumber = if validGroup then  getChildren(stringOfParent, connections, []) else []
-              remainingConnections = filter (\(key, communicators) -> key `notElem` foundGroupOfCurrentNumber) connections
+              remainingConnections = filter (\connection -> fst connection `notElem` foundGroupOfCurrentNumber) connections
               newAmountOfGroups = totalGroups + if validGroup then 1 else 0
 
 getChildren :: (String, [(String,[String])], [String]) -> [String]
 getChildren (searchParent, communications, foundAlready) = nub allChildren
-        where (p, directChildren) = head (filter (\connection -> isKeyEqualToDesiredKey(searchParent,connection)) communications)
-              thosefound = append [searchParent] foundAlready
-              newChildrenToSearch = filter (`notElem` thosefound) directChildren
-              allChildren = foldr (append . (\child -> getChildren(child, communications, thosefound))) directChildren newChildrenToSearch
+        where directChildren = snd (head (filter (\connection -> isKeyEqualToDesiredKey(searchParent,connection)) communications))
+              thoseFound = append [searchParent] foundAlready
+              newChildrenToSearch = filter (`notElem` thoseFound) directChildren
+              allChildren = foldr (append . (\child -> getChildren(child, communications, thoseFound))) directChildren newChildrenToSearch
 
 getInitialAdjacencyLists :: [String] -> [(String,[String])]
 getInitialAdjacencyLists assignmentStatements = communications
         where assignmentStatementsNumberLists = map (words . filter (\character -> isDigit character || isSpace character)) assignmentStatements
               communications = map (\assignmentNumbers -> (head assignmentNumbers,tail assignmentNumbers)) assignmentStatementsNumberLists
 
-isKeyEqualToDesiredKey :: (String, (String, [String])) -> Bool 
+isKeyEqualToDesiredKey :: (String, (String, [String])) -> Bool
 isKeyEqualToDesiredKey (desiredKey, (key, children)) = key == desiredKey
 
 main :: IO()
 main = do inputText <- readFile day12FilePath
-          part1 $ lines inputText
+          part2 $ lines inputText
